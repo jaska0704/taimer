8e2695fc11a97b4c4b7b1f32f9222b55773fa5bf
Jasurbek <jaskasamadov695@gmail.com>
2023-12-19 16:25:37 +0500

    mabbgvsgvsvjhvj

--- Js/soat.js
+++ Js/soat.js
@@ -10,7 +10,6 @@
 // let elements = document.querySelector(".maxsus");//yangi usul, barchasidan birdek foydalansa bo'ladi
 // let elements = document.querySelectorAll("body");//barchasiga birdek ta'sir qiladi
 
-
 // //changing styles
 // btn.style.backgroundColor = "black";
 // btn.style.color = "red"
@@ -25,7 +24,7 @@
 
 //         let clock = hours + ":" + minutes + ":" + seconds;
 //         document.querySelector("#clock").innerHTML = clock;
-//     }); 
+//     });
 // }
 
 let minut = document.querySelector(".minut");
@@ -37,25 +36,24 @@ let count = 0;
 let count1 = 0;
 let count2 = 0;
 
-
 let sek2 = setInterval(() => {
-    ++count2;
-    if (count2===10){
-        count2=0
-    }
-    sekundcha.innerHTML = count2;
+  ++count2;
+  if (count2 === 10) {
+    count2 = 0;
+  }
+  sekundcha.innerHTML = count2 < 10 ? "0" + count2 : count2;
 }, 100);
 let sek = setInterval(() => {
-    ++count;
-    if (count===60){
-        count=0
-    }
-    sekund.innerHTML = count;
+  ++count;
+  if (count === 60) {
+    count = 0;
+  }
+  sekund.innerHTML = count < 10 ? "0" + count : count;
 }, 1000);
 let min = setInterval(() => {
-    ++count1;
-    if (count1===60){
-        count1=0
-    }
-    minut.innerHTML = count1;
+  ++count1;
+  if (count1 === 60) {
+    count1 = 0;
+  }
+  minut.innerHTML = count1 < 10 ? "0" + count1 : count1;
 }, 60000);
