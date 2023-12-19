4eafacdbbec31837fa97c780286b9004edf11c5b
Jasurbek <jaskasamadov695@gmail.com>
2023-12-19 17:17:44 +0500

    mabbgvsgvsvjhvjjhh

--- Js/svetafor.js
+++ Js/svetafor.js
@@ -1,40 +1,40 @@
-// let arr = [
-//     {id: 1, name: "Temur", age: 23, title: "Frontend"},
-//     {id: 2, name: "Bobur", age: 26, title: "Backend"},
-//     {id: 3, name: "Rashid", age: 30, title: "Flutter"},
-//     {id: 4, name: "Jamshid", age: 40, title: "Android"},
-//     {id: 5, name: "Islom", age: 29, title: "Ios"},
-//     {id: 6, name: "Islom", age: 39, title: "Bios"},
-//     {id: 7, name: "Sohibjon", age: 50, title: "Devops"}
-// ]
+let red = document.querySelector(".red");
+let yellow = document.querySelector(".yellow");
+let green = document.querySelector(".green");
 
-// console.log(arr.filter((item, index, arr) => item.name === "Islom"));
-
-function TrafficLight() {
-    const red = document.querySelector(".red");
-    const yellow = document.querySelector(".yellow");
-    const green = document.querySelector(".green");
-
-    function Red() {
-        red.style.backgroundColor = "red";
-        yellow.style.backgroundColor = "rgb(93, 99, 92)";
-        green.style.backgroundColor = "rgb(93, 99, 92)";
-        setTimeout(Yellow, 4000)
-    }
-    function Yellow() {
-        red.style.backgroundColor = "rgb(93, 99, 92)";
-        yellow.style.backgroundColor = "yellow";
-        green.style.backgroundColor = "rgb(93, 99, 92)";
-        setTimeout(Green, 2000)
-    }
-    function Green() {
-        red.style.backgroundColor = "rgb(93, 99, 92)";
-        yellow.style.backgroundColor = "rgb(93, 99, 92)";
-        green.style.backgroundColor = "green";
-        setTimeout(Red, 4000)
-    }
-
-    Red();
+function wait(time) {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res();
+    }, time);
+  });
 }
 
-TrafficLight();
+function redLight() {
+  red.classList.add("on");
+  yellow.classList.remove("on");
+  green.classList.remove("on");
+}
+function yellowLight() {
+  red.classList.remove("on");
+  yellow.classList.add("on");
+  green.classList.remove("on");
+}
+function greenLight() {
+  red.classList.remove("on");
+  yellow.classList.remove("on");
+  green.classList.add("on");
+}
+
+async function sr() {
+  redLight();
+  await wait(3000);
+  yellowLight();
+  await wait(1000);
+  greenLight();
+  await wait(3000);
+  yellowLight();
+}
+
+sr();
+setInterval(sr, 8_000);
